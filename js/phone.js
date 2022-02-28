c4431a4d4b10a9f9e682629a7603107a4c3d55f8
Foysal Ahamed Badsha <you@example.com>
2022-03-01 02:06:39 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,29 @@
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(result => displaySearchResult(result.data));
+}
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-iphone');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="${data.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${data.phone_name}</h5>
+        <p class="card-text">${data.slug}</p>
+        </div>
+        </div>
+        `;
+        searchResult.appendChild(div)
+    })
+}
