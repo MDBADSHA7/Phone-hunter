21611c7e42a02a753d55c1e8da33f414d8d0b9ad
Foysal Ahamed Badsha <you@example.com>
2022-03-02 12:45:26 +0600

    first commit

--- js/phone.js
+++ js/phone.js
@@ -16,12 +16,12 @@ const displaySearchResult = data => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadPhoneDetail('${data.brand}')" class="card h-100">
         <img src="${data.image}" class="card-img-top" alt="...">
         <div class="card-body">
+        <h4 class="phone-brand">${data.brand}</h4>
         <h5 class="card-title">${data.phone_name}</h5>
         <p class="card-text">${data.slug}</p>
-        <button onclick="searchPhone()" class="bg-danger text-white">Details</button>
+        <button onclick="loadPhoneDetail('${data.brand}')" class="btn btn-success bg-danger text-white">Details</button>
         </div>
         </div>
         `;
@@ -29,8 +29,9 @@ const displaySearchResult = data => {
     })
 }
 const loadPhoneDetail = brand => {
-    const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089=${brand}`;
+    const url = `https://openapi.programming-hero.com/api/phone/${brand}`;
     fetch(url)
         .then(res => res.json())
         .then(final => console.log(final.data));
 }
+loadPhoneDetail();
